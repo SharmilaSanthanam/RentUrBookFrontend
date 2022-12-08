dc8104da8ab41031269f4d0bd69d697ff44779ba
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-12-08 15:18:02 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://renturbookmern.herokuapp.com",
+//     baseURL: "https://renturbookmern.herokuapp.com",
+     baseURL: "https://renturbook.onrender.com",
+    
 });
 
 export default instance;
